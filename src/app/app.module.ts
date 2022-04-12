cc0b05797f7f26c00ba5062c8b807a34dbabdd2c
torvic886 <vagutierrezv@uqvirtual.edu.co>
2022-04-12 17:36:04 -0500

    se agregan estilos, rutas y cambios en los componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminUsuarioComponent } from './pages/admin-usuario/admin-usuario.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminUsuarioComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
